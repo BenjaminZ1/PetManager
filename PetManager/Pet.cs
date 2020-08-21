247f7aa43b4827f8c3761b58b8ee3eeee169452a
Benjamin Zech <benjamin.zech@ari-ag.ch>
2020-08-21 12:11:01 +0200

    added gethashcode to pet

--- PetManager/Pet.cs
+++ PetManager/Pet.cs
@@ -8,5 +8,15 @@ namespace PetManager {
     public class Pet {
         public string Name { get; set; }
         public string Breed { get; set; }
+
+
+        public int GetHashCode(Pet obj)
+        {
+            unchecked
+            {
+                return ((obj.Name != null ? obj.Name.GetHashCode() : 0) * 397) ^ (obj.Breed != null ? obj.Breed.GetHashCode() : 0);
+            }
+        }
+
     }
 }
